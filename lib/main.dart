16782d733bd8fd361b064900d4dd585bedc7ba55
Aaryan Arora <aaryan2134@gmail.com>
2023-12-02 21:40:04 +0000

    Made the changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,211 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => RepositoryProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'GitHub API App',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyHomePage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GitHub Repositories'),
+      ),
+      body: Consumer<RepositoryProvider>(
+        builder: (context, provider, _) {
+          if (provider.isLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (provider.hasError) {
+            return Center(
+              child: Text('Error loading repositories'),
+            );
+          } else {
+            return RepositoryList(provider.repositories);
+          }
+        },
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class RepositoryList extends StatelessWidget {
+  final List<Repository> repositories;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  RepositoryList(this.repositories);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ListView.builder(
+      itemCount: repositories.length,
+      itemBuilder: (context, index) {
+        return RepositoryListItem(repositories[index]);
+      },
     );
   }
 }
+
+class RepositoryListItem extends StatelessWidget {
+  final Repository repository;
+
+  RepositoryListItem(this.repository);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(repository.name),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(repository.description),
+          Text('Visibility: ${repository.visibility}'),
+          Text('Forks: ${repository.forks}'),
+          Text('Open Issues: ${repository.openIssues}'),
+          Text('Watchers: ${repository.watchers}'),
+          Text('Default Branch: ${repository.defaultBranch}'),
+          if (repository.lastCommitSha.isNotEmpty)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Last Commit: ${repository.lastCommitMessage}'),
+                Text('Author: ${repository.lastCommitAuthor}'),
+                Text('Date: ${repository.lastCommitDate.toLocal()}'),
+              ],
+            ),
+        ],
+      ),
+      onTap: () {
+        Provider.of<RepositoryProvider>(context, listen: false).fetchLastCommit(repository);
+        // Add navigation or any other action when a repository is tapped
+      },
+      trailing: Icon(Icons.arrow_forward),
+    );
+  }
+}
+
+
+
+class RepositoryProvider with ChangeNotifier {
+  List<Repository> _repositories = [];
+  bool _isLoading = true;
+  bool _hasError = false;
+
+  RepositoryProvider() {
+    fetchRepositories();
+  }
+
+  List<Repository> get repositories => _repositories;
+  bool get isLoading => _isLoading;
+  bool get hasError => _hasError;
+
+  Future<void> fetchRepositories() async {
+  try {
+    final response = await http.get(Uri.parse('https://api.github.com/users/freeCodeCamp/repos'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      _repositories = data.map((repo) => Repository.fromJson(repo)).toList();
+    } else {
+      print('Error loading repositories. Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      _hasError = true;
+    }
+  } catch (e, stackTrace) {
+    print('Error loading repositories: $e');
+    print('Stack trace: $stackTrace');
+    _hasError = true;
+  } finally {
+    _isLoading = false;
+    notifyListeners();
+  }
+}
+
+Future<void> fetchLastCommit(Repository repo) async {
+    try {
+      final response = await http.get(Uri.parse('https://api.github.com/repos/freeCodeCamp/${repo.name}/commits'));
+
+      if (response.statusCode == 200) {
+        List<dynamic> commits = json.decode(response.body);
+        if (commits.isNotEmpty) {
+          Map<String, dynamic> lastCommit = commits.first['commit'];
+          repo.lastCommitSha = commits.first['sha'] ?? '';
+          repo.lastCommitMessage = lastCommit['message'] ?? '';
+          repo.lastCommitAuthor = lastCommit['author']['name'] ?? '';
+          repo.lastCommitDate = DateTime.parse(lastCommit['author']['date'] ?? '');
+        }
+      } else {
+        print('Error loading last commit for ${repo.name}. Status code: ${response.statusCode}');
+      }
+    } catch (e, stackTrace) {
+      print('Error loading last commit for ${repo.name}: $e');
+      print('Stack trace: $stackTrace');
+    } finally {
+      notifyListeners();
+    }
+  }
+
+}
+
+class Repository {
+  final String name;
+  final String description;
+  final String visibility;
+  final int forks;
+  final int openIssues;
+  final int watchers;
+  final String defaultBranch;
+  String lastCommitSha;
+  String lastCommitMessage;
+  String lastCommitAuthor;
+  DateTime lastCommitDate;
+
+  Repository({
+    required this.name,
+    required this.description,
+    required this.visibility,
+    required this.forks,
+    required this.openIssues,
+    required this.watchers,
+    required this.defaultBranch,
+    required this.lastCommitSha,
+    required this.lastCommitMessage,
+    required this.lastCommitAuthor,
+    required this.lastCommitDate,
+  });
+
+  factory Repository.fromJson(Map<String, dynamic> json) {
+    return Repository(
+      name: json['name'] ?? '',
+      description: json['description'] ?? '',
+      visibility: json['visibility'] ?? 'public',
+      forks: json['forks'] ?? 0,
+      openIssues: json['open_issues'] ?? 0,
+      watchers: json['watchers'] ?? 0,
+      defaultBranch: json['default_branch'] ?? 'main',
+      lastCommitSha: '', // initialize to empty string
+      lastCommitMessage: '',
+      lastCommitAuthor: '',
+      lastCommitDate: DateTime.now(), // initialize to current date/time
+    );
+  }
+}
+
+
